fb089ca1480235019317154b974b194d5f014238
VADSGroup16 <155652093+VADSGroup16@users.noreply.github.com>
2024-01-04 20:53:46 +0530

    Update recruit_vads.py

--- recruit_vads.py
+++ recruit_vads.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1zd5LrFvNJtdiukhv8FCXAnoCILazJFDB
 """
 
-!pip install streamlit
 import streamlit as st
 
 # Define the layout of the app

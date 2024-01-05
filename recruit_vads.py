92b3625a0fa5f9e715534abfd35839b8d35e459b
VADSGroup16 <155652093+VADSGroup16@users.noreply.github.com>
2024-01-05 14:09:29 +0530

    Update recruit_vads.py

--- recruit_vads.py
+++ recruit_vads.py
@@ -11,7 +11,7 @@ def fetch_candidates(role, experience, skills):
     })
 
 # Set page configuration
-st.set_page_config(page_title="Recruit VADS", layout="wide")
+st.set_page_config(page_title="Recruit VADS")
 
 # Display the header image in the middle of the page
 header_image_path = "Header.png"  # Path to the header image file

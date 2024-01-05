0f8ee46d7a6be796ad5dc1a4c805063eba594422
VADSGroup16 <155652093+VADSGroup16@users.noreply.github.com>
2024-01-05 13:54:46 +0530

    Update recruit_vads.py

--- recruit_vads.py
+++ recruit_vads.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Recruit VADS", layout="wide")
 
 # Display the header image in the middle of the page
 header_image_path = "Header.png"  # Path to the header image file
-st.image(header_image_path, width=400)  # Adjust the width to make the image smaller
+st.image(header_image_path, width=700)  # Adjust the width to make the image smaller
 # Create a two-column layout for the form and candidates table
 col1, col2 = st.columns([1, 2])
 

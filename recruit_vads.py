e32781a158d504c92eed1dfcd8b0edd75f1f8a1c
VADSGroup16 <155652093+VADSGroup16@users.noreply.github.com>
2024-01-11 15:57:50 +0530

    Update recruit_vads.py

--- recruit_vads.py
+++ recruit_vads.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
+!pip install scikit-learn
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 model = pickle.load(open('Recruit_VADS_model.pkl', 'rb'))

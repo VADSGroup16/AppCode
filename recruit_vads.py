09ae6a33a2a4aee95d67de26d0b86ce5db49c14a
VADSGroup16 <155652093+VADSGroup16@users.noreply.github.com>
2024-01-11 21:37:37 +0530

    Update recruit_vads.py

--- recruit_vads.py
+++ recruit_vads.py
@@ -5,16 +5,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Load the model and vectorizer
+
+def load_artifacts():
+    with open('Recruit_VADS_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+    with open('Tfidf_Vectorizer.pkl', 'rb') as vectorizer_file:
+        vectorizer = pickle.load(vectorizer_file)
+    return model, vectorizer
+
 # Load candidate data
-@st.cache(allow_output_mutation=True)
+
 def load_candidate_data():
     return pd.read_csv('Modifiedresumedata_data.csv')
 
 # Predict relevancy scores
 def predict_relevancy(vectorizer, input_data, candidate_data):
-    # Make a copy of the candidate data to avoid mutation
-    candidate_data = candidate_data.copy()
-
     combined_input = ' '.join([input_data['Role'], str(input_data['Experience']), 
                                input_data['Certifications'], input_data['Skills']])
     X_input = vectorizer.transform([combined_input])
@@ -31,7 +36,6 @@ def predict_relevancy(vectorizer, input_data, candidate_data):
     candidate_data['RelevancyScore'] = candidate_scores
     # Convert scores to percentages and round to two decimal places
     candidate_data['RelevancyScore'] = (candidate_data['RelevancyScore'] * 100).round(2)
-    
     top_candidates = candidate_data.nlargest(5, 'RelevancyScore')
     return top_candidates[['Candidate Name', 'Email ID', 'RelevancyScore']]
 

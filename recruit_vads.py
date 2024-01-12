cf04be8abfa09b7919643fa73d76f6f2ef81cda4
VADSGroup16 <155652093+VADSGroup16@users.noreply.github.com>
2024-01-12 14:23:56 +0530

    Update recruit_vads.py

--- recruit_vads.py
+++ recruit_vads.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity

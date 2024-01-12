7d60d767d3f68ad73308f73ab94974f90160b241
VADSGroup16 <155652093+VADSGroup16@users.noreply.github.com>
2024-01-12 10:30:53 +0530

    Update recruit_vads.py

--- recruit_vads.py
+++ recruit_vads.py
@@ -75,7 +75,7 @@ candidate_data = load_candidate_data()
 # Display results
 with col2:
     if st.session_state['submitted']:
-        top_candidates = predict_relevancy(model, vectorizer, user_input, candidate_data)
+        top_candidates = predict_relevancy(model, vectorizer, form_data, candidate_data)
         st.write('Top Candidate Matches:')
         st.dataframe(top_candidates)
     else:
